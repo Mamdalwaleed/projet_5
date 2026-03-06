a4d80691e55611eb0d90d85b416e31d8cb33a0a2
mamdal <waleedmamdal96@gmail.com>
2026-03-06 16:00:17 +0100

    modification de la page erreur

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import "./Error.css";
+
 function Error() {
-  return <h1>404 - Page introuvable</h1>;
+  return (
+    <div className="error-page">
+      <h1 className="error-code">404</h1>
+      <p className="error-text">
+        Oups ! La page que vous demandez n'existe pas.
+      </p>
+
+      <Link to="/" className="error-link">
+        Retourner à l’accueil
+      </Link>
+    </div>
+  );
 }
 
 export default Error;
